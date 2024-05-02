7425130b2d359e9a0e0a8bc2f15ed1e94bf74a6d
oskar <oskartryggvason@gmail.com>
2024-05-02 13:07:36 +0200

    updated gen and disc files

--- discriminator.py
+++ discriminator.py
@@ -1,31 +1,31 @@
-import torch.nn as nn
-
 class Discriminator(nn.Module):
-    def __init__(self, feature_map_size,image_channels=3):
+    def __init__(self, output_channels):
         super(Discriminator, self).__init__()
         
         self.main = nn.Sequential(
-            # input is ``(image_channels) x 64 x 64``
-            nn.Conv2d(image_channels, feature_map_size, 4, 2, 1, bias=False),
+            
+            nn.Conv2d(output_channels, 64, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. ``(feature_map_size) x 32 x 32``
-            nn.Conv2d(feature_map_size, feature_map_size * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(feature_map_size * 2),
+           
+            nn.Conv2d(64, 64 * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64 * 2),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. ``(feature_map_size*2) x 16 x 16``
-            nn.Conv2d(feature_map_size * 2, feature_map_size * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(feature_map_size * 4),
+
+            nn.Conv2d(64*2, 64 * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64 * 2),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. ``(feature_map_size*4) x 8 x 8``
-            nn.Conv2d(feature_map_size * 4, feature_map_size * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(feature_map_size * 8),
+           
+            nn.Conv2d(64 * 2, 64 * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64 * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+           
+            nn.Conv2d(64 * 4, 64 * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64 * 8),
             nn.LeakyReLU(0.2, inplace=True),
             
-            nn.Conv2d(feature_map_size * 8, feature_map_size * 16, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(feature_map_size * 16),
-            nn.LeakyReLU(0.2, inplace=True),
-            # state size. ``(feature_map_size*8) x 4 x 4``
-            nn.Conv2d(feature_map_size * 16, 1, 4, 1, 0, bias=False),
+            
+            
+            nn.Conv2d(64 * 8, 1, 4, 1, 0, bias=False),
             nn.Sigmoid()
         )
 
--- generator.py
+++ generator.py
@@ -1,34 +1,100 @@
-import torch.nn as nn
 class Generator(nn.Module):
-    def __init__(self,input_size ,size_feature_map, image_channels = 3):
+    def __init__(self, in_channels, out_channels):
         super(Generator, self).__init__()
-        
-        self.main = nn.Sequential(
-            # input is Z, going into a convolution
-            nn.ConvTranspose2d( input_size, size_feature_map * 16, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(size_feature_map * 16),
-            nn.ReLU(True),
+
+        # downsize image (Encoder)
+        self.encoder = nn.Sequential(
+            nn.Conv2d(in_channels, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2), #downsampling
             
-            nn.ConvTranspose2d( size_feature_map * 16, size_feature_map * 8, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(size_feature_map * 8),
-            nn.ReLU(True),
-            # state size. ``(size_feature_map*8) x 4 x 4``
-            nn.ConvTranspose2d(size_feature_map * 8, size_feature_map * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(size_feature_map * 4),
-            nn.ReLU(True),
-            # state size. ``(size_feature_map*4) x 8 x 8``
-            nn.ConvTranspose2d( size_feature_map * 4, size_feature_map * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(size_feature_map * 2),
-            nn.ReLU(True),
-            # state size. ``(size_feature_map*2) x 16 x 16``
-            nn.ConvTranspose2d( size_feature_map * 2, size_feature_map, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(size_feature_map),
-            nn.ReLU(True),
-            # state size. ``(size_feature_map) x 32 x 32``
-            nn.ConvTranspose2d( size_feature_map, image_channels, 4, 2, 1, bias=False),
-            nn.Tanh()
-            # state size. ``(nc) x 64 x 64``
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2), #downsampling
+            
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+        )
+        self.encoder1 = nn.Sequential(
+            nn.Conv2d(in_channels, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2), #downsampling
+    
+        )
+        self.encoder2 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2), #downsampling
+    
+        )
+        self.encoder3 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2), #downsampling
+    
+        )
+        # upsize  image (Decoder)
+        self.decoder = nn.Sequential(
+            
+            nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2),
+            nn.BatchNorm2d(128),  # Upsampling
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            
+            
+            nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2),  # Upsampling
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            
+        )
+        self.decoder1 = nn.Sequential(
+            
+            
+            nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),  # Upsampling
+            nn.ReLU(inplace=True),
+        )
+        self.decoder2 = nn.Sequential(
+            
+            
+            nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),  # Upsampling
+            nn.ReLU(inplace=True),
+        )
+        self.decoder3 = nn.Sequential(
+            
+            nn.ConvTranspose2d(64, 64, kernel_size=2, stride=2),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),  # Upsampling
+            nn.ReLU(inplace=True),
         )
 
-    def forward(self, input):
-        return self.main(input)
+        # Output layer
+        self.output = nn.Conv2d(64, out_channels, kernel_size=3, padding=1)
+
+    def forward(self, x):
+        
+        x1 = self.encoder1(x)
+        x2 = self.encoder2(x1)
+        x3 = self.encoder3(x2)
+        
+        
+        x = self.decoder1(x3) 
+        x = self.decoder2(x + x2) #added skipconnection
+        x = self.decoder3(x + x1) #added skipconnection
+        
+        x3 = self.output(x)
+        return x3
+
